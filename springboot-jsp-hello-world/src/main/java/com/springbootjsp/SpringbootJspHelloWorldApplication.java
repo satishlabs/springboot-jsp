4381fae23ab528c18c2a29cdefd9417efdac0778
Satish Prasad <spd61185@gmail.com>
2020-05-06 13:11:42 +0530

    Spring Boot JSP Hello World Example Tutorial

---
+++ springboot-jsp-hello-world/src/main/java/com/springbootjsp/SpringbootJspHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.springbootjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJspHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJspHelloWorldApplication.class, args);
+	}
+
+}
---
+++ springboot-jsp-hello-world/src/test/java/com/springbootjsp/SpringbootJspHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJspHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
